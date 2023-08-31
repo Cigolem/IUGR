2ad039c6fa6a76df75b2ee3ce249c56842e72bad
Cigdem <71763132+Cigolem@users.noreply.github.com>
2023-08-31 06:34:25 +0100

    Add files via upload

--- REM_series.m
+++ REM_series.m
@@ -1,6 +1,9 @@
-
+% This script is used to generate multiple "epochs" each lasting 30 sec. from single REM time series
+% The input file is the "REM_single.mat" 
+% The output file is the "REM_multiple.mat"
 
 %% load cell array that contains time series from REM states (these cell arrays are called "REM_single")
+load('C:\Users\Cigdem\MATLAB Drive\baby EEG\Baby Files\scripts for manuscript\GITHUB\REM_single.mat')
 
 %% Organize single time series into epoched arrays each epoch lasting 30 sec (15000 samples)
 
@@ -10,13 +13,16 @@ for i = 1:length(REM_single.REM_T7)
         REM_T7 (i) = REM_single.REM_T7(i,1);
 end
 
-% organize single arrays into a matrix with 30 sec multiple trials 
+% organize single arrays into a matrix with multiple epochs each lasting 30 sec. (i.e. 15000 samples)
+srate = 500; % sample rate
+window = 30; % analysis window (in sec)
+epoch_length = srate * window; % analysis window (sample number)
 
 for i = 1: length(REM_T7) 
-      irange = 1:15000:length(REM_T7{1,i})-15000;
+      irange = 1:epoch_length:length(REM_T7{1,i})-epoch_length;
          for j=1:length(irange)
              m = irange(j);
-        REM_T7m {1,i}(j,:)  = REM_T7{1,i}(m:(m+14999));
+        REM_T7m {1,i}(j,:)  = REM_T7{1,i}(m:(m+(epoch_length-1)));
          end
      
 end
@@ -36,10 +42,10 @@ end
 % organize single arrays into a matrix with 30 sec multiple trials 
 
 for i = 1: length(REM_T8) 
-      irange = 1:15000:length(REM_T8{1,i})-15000;
+      irange = 1:epoch_length:length(REM_T8{1,i})-epoch_length;
          for j=1:length(irange)
              m = irange(j);
-        REM_T8m {1,i}(j,:)  = REM_T8{1,i}(m:(m+14999));
+        REM_T8m {1,i}(j,:)  = REM_T8{1,i}(m:(m+(epoch_length-1)));
          end
      
 end
@@ -57,11 +63,8 @@ REM_multiple = output;
 
 clearvars -except REM_multiple
 
-% save REM_multiple structure array for the next step
+% save REM_multiple structure array for further analyses 
+save('C:\Users\Cigdem\MATLAB Drive\baby EEG\Baby Files\scripts for manuscript\GITHUB\REM_multiple.mat');
 
-%% end 
-
-
-
-    
+%% end     
     
