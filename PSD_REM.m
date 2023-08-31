6cef8d0735b583cec3dd8ea0f32cf3d354a93c93
Cigdem <71763132+Cigolem@users.noreply.github.com>
2023-08-31 10:34:49 +0100

    Add files via upload
    This script is used to generate Power spectral density estimates of multiple REM epochs from temporal channels

---
+++ PSD_REM.m
@@ -0,0 +1,47 @@
+
+% This script is used to generate Power spectral density estimates of multiple REM epochs from temporal channels 
+
+% The input is the workspace where all individual REM_multiple time series from all subjects are stored 
+% The output is:
+           % freq_PSD array (frequency array for plotting power spectral density estimates)
+           % PSD_Control (cell array that contains PSD estimates for all epochs from T7/T8 channels separately for each subject) 
+           % PSD_Control_av (cell array that contains PSD estimates for all epochs average of T7/T8 channels for each subject) 
+
+
+%% Load the workspace where all individual multiple REM time series are stored and store all series in a cell array
+
+REM_Control = (struct2cell(load('C:\Users\Cigdem\MATLAB Drive\baby EEG\Baby Files\scripts for manuscript\GITHUB\REM_Control.mat')))';
+
+for i = 1:length(REM_Control)
+    REM_Control{1,i} = struct2cell(REM_Control{1,i});
+end
+
+
+%% Perform PSD using the script "PSD_CG" for each channel separately
+for i = 1: length(REM_Control)
+    for j = 1:length(REM_Control{1,i})
+        for k = 1:size(REM_Control{1,i}{j,1}, 1)
+    PSD_Control {1,i}{j,1}(k,:) = PSD_CG (REM_Control{1,i}{j,1}(k,:));
+        end
+    end
+end
+
+clearvars i j k
+
+% Save PSD_Control (or PSD_Exp) - this contains PSD from T7 and T8 separate
+save('C:\Users\Cigdem\MATLAB Drive\baby EEG\Baby Files\scripts for manuscript\GITHUB\PSD_Control.mat');
+
+% Generate frequency array to use for plotting and save 
+[~ , freq_PSD] = PSD_CG(REM_Control{1,1}{1,1}(1,:));
+save('C:\Users\Cigdem\MATLAB Drive\baby EEG\Baby Files\scripts for manuscript\GITHUB\freq_PSD.mat');
+
+% Average PSDs from T7 and T8
+for i = 1:length(PSD_Control)
+    PSD_Control_av {1,i} = ((PSD_Control{1,i}{1,1}) + (PSD_Control{1,i}{2,1}))/2;
+end
+
+save('C:\Users\Cigdem\MATLAB Drive\baby EEG\Baby Files\scripts for manuscript\GITHUB\PSD_Control_av.mat');
+
+% the arrays "freq_PD" and "PSD_Control_av" will be used for the script "PSD_plotting" 
+
+%% end
